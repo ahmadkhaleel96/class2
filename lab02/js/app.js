d34d2cf3acaa1fa576543c570051f117f3ba3142
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2019-11-11 15:21:09 +0200

    done

---
+++ lab02/js/app.js
@@ -0,0 +1,42 @@
+'use strict'
+var name = prompt("Give me that amazing Name of yours");
+alert(name + " What a lovely name indeed! you should be proud of it!!");
+
+confirm("now it's time to use that pink thing of yours (in your skull!) and let us have a Know About me (ME THE OWNER NOT YOU !!) game ...you have no choice bro. please anwser the folowing questions using yes/no or y/n")
+
+var weird = prompt("am i a weird person?");
+// console.log(weird.toUpperCase())
+if (weird.toUpperCase() == "YES" || weird.toUpperCase() == "Y") {
+    alert("that is correct!! I am weird just super weird...")
+} else {
+    alert(" you don't know me do you? based on a very long and complicated studies and forums it is proven that i am wierd")
+}
+var nerd = prompt("am i a movie, series, anime Nerd?");
+// console.log(nerd.toUpperCase())
+if (nerd.toUpperCase() == "YES" || nerd.toUpperCase == "Y") {
+    alert("that is correct!!, i am the biggest movies, series and anime geek in this plant!!")
+} else{
+    alert(" i guess watching 131 movies, 2 series, 5 Animee. in a 25 days does not qualify me to be a nerd IDK!!")
+}
+var like = prompt("do i like people?");
+// console.log(like.toUpperCase())
+if (like.toUpperCase() == "NO" || like.toUpperCase() == "N") {
+    alert("yeah i don't like people very much, but the frame rate, sharpness, drawing and frame smoothness always intreset me!!")
+} else{
+    alert("REALLY, lets us think for a bit if i like people why would i do a Know About me game on a random website")
+}
+var habit = prompt("am i a creature of habit?");
+// console.log(habit.toUpperCase())
+if (habit.toUpperCase() == "NO" || habit.toUpperCase() == "N") {
+    alert("NO I am not, I hate routine very much because it's a creativity killer!!!")
+} else{
+    alert("i love my creativity!!! I AM NOT a routine guy!!")
+}
+var film = prompt("do i love filmmaking?");
+// console.log(film.toUpperCase())
+if (film.toUpperCase() == "YES" || film.toUpperCase() == "Y") {
+    alert("Filmmaking is MY TRUE passion i just love it, and photography too!!")
+} else{
+    alert("dude, you should read and develope your mind and train with those bald monk dudes, to know i love filmmaking!!")
+}
+alert(name + " thank you for answering, it is good to know people even through websites (which is very sad go get a life for the love of GOD and spend your night sharing feeling not answering some stranger dude on the internet about himself!!)");
