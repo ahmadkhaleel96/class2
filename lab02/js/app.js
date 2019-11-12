72a86c510d50a5c60efaf25e0bd7219ad819144b
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2019-11-12 20:55:28 +0200

    added more anformation about me and more questins that are a guess the number and a question with multiple answers, using if , for and array to achieve that , no CSS is used or changed in this project

--- lab02/js/app.js
+++ lab02/js/app.js
@@ -1,4 +1,5 @@
 'use strict'
+var grade =0;
 var name = prompt("Give me that amazing Name of yours");
 alert(name + " What a lovely name indeed! you should be proud of it!!");
 
@@ -7,36 +8,77 @@ confirm("now it's time to use that pink thing of yours (in your skull!) and let 
 var weird = prompt("am i a weird person?");
 // console.log(weird.toUpperCase())
 if (weird.toUpperCase() == "YES" || weird.toUpperCase() == "Y") {
-    alert("that is correct!! I am weird just super weird...")
+    grade= grade + 1;
+    alert("that is correct!! I am weird just super weird... your grade is: " + grade);
 } else {
-    alert(" you don't know me do you? based on a very long and complicated studies and forums it is proven that i am wierd")
+    alert(" you don't know me do you? based on a very long and complicated studies and forums it is proven that i am wierd ... your grade is: " + grade);
 }
 var nerd = prompt("am i a movie, series, anime Nerd?");
 // console.log(nerd.toUpperCase())
-if (nerd.toUpperCase() == "YES" || nerd.toUpperCase == "Y") {
-    alert("that is correct!!, i am the biggest movies, series and anime geek in this plant!!")
+if (nerd.toUpperCase() == "YES" || nerd.toUpperCase() == "Y") {
+    grade = grade + 1 ;
+    alert("that is correct!!, i am the biggest movies, series and anime geek in this plant!! ... your grade is: " + grade);
 } else{
-    alert(" i guess watching 131 movies, 2 series, 5 Animee. in a 25 days does not qualify me to be a nerd IDK!!")
+    alert(" i guess watching 131 movies, 2 series, 5 Animee. in a 25 days does not qualify me to be a nerd IDK!!... you grade is: " + grade);
 }
 var like = prompt("do i like people?");
 // console.log(like.toUpperCase())
 if (like.toUpperCase() == "NO" || like.toUpperCase() == "N") {
-    alert("yeah i don't like people very much, but the frame rate, sharpness, drawing and frame smoothness always intreset me!!")
+    grade = grade + 1;
+    alert("yeah i don't like people very much, but the frame rate, sharpness, drawing and frame smoothness always intreset me!! your grade is: " + grade);
 } else{
-    alert("REALLY, lets us think for a bit if i like people why would i do a Know About me game on a random website")
+    alert("REALLY, lets us think for a bit if i like people why would i do a Know About me game on a random website your grade is: " + grade);
 }
 var habit = prompt("am i a creature of habit?");
 // console.log(habit.toUpperCase())
 if (habit.toUpperCase() == "NO" || habit.toUpperCase() == "N") {
-    alert("NO I am not, I hate routine very much because it's a creativity killer!!!")
+    grade = grade +1;
+    alert("NO I am not, I hate routine very much because it's a creativity killer!!!.... your grade is: " + grade);
 } else{
-    alert("i love my creativity!!! I AM NOT a routine guy!!")
+    alert("i love my creativity!!! I AM NOT a routine guy!! ... your grade is: " + grade);
 }
 var film = prompt("do i love filmmaking?");
 // console.log(film.toUpperCase())
 if (film.toUpperCase() == "YES" || film.toUpperCase() == "Y") {
-    alert("Filmmaking is MY TRUE passion i just love it, and photography too!!")
+    grade = grade + 1;
+    alert("Filmmaking is MY TRUE passion i just love it, and photography too!! ...your grade is: " + grade);
 } else{
-    alert("dude, you should read and develope your mind and train with those bald monk dudes, to know i love filmmaking!!")
+    alert("dude, you should read and develope your mind and train with those bald monk dudes, to know i love filmmaking!! your grade is: " + grade);
 }
-alert(name + " thank you for answering, it is good to know people even through websites (which is very sad go get a life for the love of GOD and spend your night sharing feeling not answering some stranger dude on the internet about himself!!)");
+
+var guess = prompt("guess a number between 1 and 10");
+var x = 0;
+for ( var i=0; i<=2 ; i++ ){
+
+if  (guess <= 5 && guess >= 1){
+    guess = prompt("too low");
+} else if (guess >=7 && guess <=10){
+    guess =prompt("too high");
+} else if(guess == 6){
+    grade = grade +1;
+    alert("you are correct!... your grade is: " + grade);
+    i = 4;
+} else {
+   guess = prompt("please choose a number betwwen 1 and 10");
+}
+
+var x = i;}
+if (x == 2){
+    alert("the answer is: 6... your grade is: " + grade);
+}
+var arr = ['architecture','music','drawing','sculpture','poetry','dance','cinema'];
+var art = prompt('name one of the seven arts');
+var arts = art.toLowerCase();
+for (var i = 0; i <=4; i++){
+ if (arts == arr[0] || arts == arr[1] || arts == arr[2] || arts == arr[3] || arts == arr[4] || arts == arr[5] || arts == arr[6]) {
+     grade = grade +1; 
+     alert('you are correct! ' + arts + ' is a bueatiful art...your grade is :' + grade)
+i = 10
+    } else {
+    prompt('sorry incorrect answer please think harder... OR I SWEAR TO GOD I WILL JUMP OUT OF THE SCREEN AND JUST STAIR AT YOU!!!... your grade is: ' + grade);
+
+ }
+if (i ==4){
+    alert("it seems that you don't know the answer, LIKE WHAT THE HELL!!!! LIKE WHAT!!! anyway the answers are: architecture, music, drawing, sculpture, poetry, dance and cinema. ... your grade is:" + grade );
+}}
+document.write('your grade is: ' + grade + ' out of 7');
